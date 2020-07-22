5b102bdda47af21330a59cbb9d18bd5453ff0d45
Leonardo Souza <leosouza@haacademy.co>
2020-07-22 07:24:55 -0700

    testes

--- CaixaEconomica.Beneficio.Testes.Domain/Entidades/Pessoa/PessoaTemPeloMenosUmEnderecoValido.cs
+++ CaixaEconomica.Beneficio.Testes.Domain/Entidades/Pessoa/PessoaTemPeloMenosUmEnderecoValido.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using CaixaEconomica.Beneficio.Dominio.Entidades;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace CaixaEconomica.Beneficio.Testes.Dominio.Entidades.Pessoa
@@ -8,10 +10,21 @@ namespace CaixaEconomica.Beneficio.Testes.Dominio.Entidades.Pessoa
     [TestClass]
     public class PessoaTemPeloMenosUmEnderecoValido
     {
-        [TestMethod]
-        public void Teste()
-        {
+        private Beneficio.Dominio.Entidades.Pessoa pessoa;
+        private Endereco endereco;
 
+        [TestInitialize]
+        public void Init()
+        {
+            pessoa = new Beneficio.Dominio.Entidades.Pessoa();
+            endereco = new Endereco();
+            pessoa.AdicionarEndereco(endereco);
+
+        }
+        [TestMethod]
+        public void PessoaTemEnderecoValido()
+        {
+            Assert.IsTrue(pessoa.Enderecos.Any());
         }
     }
 }
