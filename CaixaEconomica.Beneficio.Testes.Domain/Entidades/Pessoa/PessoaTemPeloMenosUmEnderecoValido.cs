f83478b4ac4f0ff787b8ec757bb0be99fb08ca5e
Leonardo Souza <leosouza@haacademy.co>
2020-07-22 06:37:13 -0700

    estrutura teste

---
+++ CaixaEconomica.Beneficio.Testes.Domain/Entidades/Pessoa/PessoaTemPeloMenosUmEnderecoValido.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CaixaEconomica.Beneficio.Testes.Dominio.Entidades.Pessoa
+{
+    [TestClass]
+    public class PessoaTemPeloMenosUmEnderecoValido
+    {
+        [TestMethod]
+        public void Teste()
+        {
+
+        }
+    }
+}
